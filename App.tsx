b7334d26b9ca846c4a99d541dafba8cc7503b59f
pedrotnascimento@gmail.com <pedrotnascimento@gmail.com>
2023-07-18 02:20:52 -0300

    tracking like a sonar

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@
 import React, { useEffect, useState } from 'react';
 import {
   Animated,
+  Easing,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -16,7 +17,7 @@ import {
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 const catIcon = <MaterialCommunityIcons name={"cat"} size={128} />;
-const searchingIcon = <MaterialCommunityIcons name={"map-marker-question-outline"} size={128}  color={"green"}/>;
+const searchingIcon = <MaterialCommunityIcons name={"map-marker-question-outline"} size={128} color={"green"} />;
 
 var Sound = require('react-native-sound');
 
@@ -52,7 +53,14 @@ const App = () => {
   const [playing, setPlaying] = useState(false);
   // const [soundInx, setSoundInx] = useState(randomStartingInx);
   // console.log(soundInx);
-  const [dinamycBackgroundColor, setX] = useState(new Animated.Value(0));
+  const [dynamicBackgroundColor, setX] = useState(new Animated.Value(0));
+  const [dynamicCircle, _] = useState(new Animated.Value(0));
+  // Animated.timing(dynamicCircle, {
+  //   toValue: 1,
+  //   duration: 500,
+  //   easing: Easing.linear,
+  //   useNativeDriver: false  // <-- neccessary
+  // });
 
   useEffect(() => {
     ding.setVolume(1);
@@ -61,14 +69,14 @@ const App = () => {
     };
   }, []);
 
-
+  let circleAnimationInterval;
   const play = () => {
     let currSound = catSounds[randomStartingInx];
 
     if (!playing) {
       setPlaying(true);
       Animated.spring(
-        dinamycBackgroundColor,
+        dynamicBackgroundColor,
         {
 
           toValue: 1, useNativeDriver: true,
@@ -76,6 +84,57 @@ const App = () => {
           bounciness: 100,
         }
       ).start();
+      // const toggleCircle;
+
+      console.log("new circle");
+      Animated.timing(
+        dynamicCircle,
+        {
+
+          toValue: 100,
+          useNativeDriver: true,
+          // speed: 0.001,
+          duration: 3000,
+
+          // bounciness: 100,
+
+        }
+      ).start();
+      
+      setInterval(() => {
+
+        Animated.timing(
+          dynamicCircle,
+          {
+
+            toValue: 0,
+            useNativeDriver: true,
+            // speed: 0.001,
+            duration: 1,
+
+            // bounciness: 100,
+
+          }
+        ).start(() => {
+          Animated.timing(
+            dynamicCircle,
+            {
+
+              toValue: 100,
+              useNativeDriver: true,
+              // speed: 0.001,
+              duration: 3000,
+
+              // bounciness: 100,
+
+            }
+          ).start();
+        });
+      }, 5000);
+
+
+
+
       const cb = (success: any) => {
 
         if (success) {
@@ -92,8 +151,9 @@ const App = () => {
     }
     else {
       setPlaying(false);
+      clearInterval(circleAnimationInterval);
       Animated.spring(
-        dinamycBackgroundColor,
+        dynamicBackgroundColor,
         {
 
           toValue: 0, useNativeDriver: true,
@@ -114,15 +174,21 @@ const App = () => {
     {catIcon}
     <Text style={styles.text}>Touch to find cat</Text>
   </TouchableOpacity>;
-  var color = dinamycBackgroundColor.interpolate({
+  var color = dynamicBackgroundColor.interpolate({
     inputRange: [0, 1],
     outputRange: ['rgba(255, 255, 255, 1)', 'rgba(0, 0, 0, 1)']
+  });
+
+  var bandth = dynamicCircle.interpolate({
+    inputRange: [0, 10],
+    outputRange: [1, 50]
   });
 
   return (
     // <View style={styles.container}>
     <Animated.View style={{ ...styles.container, backgroundColor: color }}>
       {playing ? FoundedButton : FindButton}
+      <Animated.View style={{ zIndex: -1, borderRadius: 50, borderWidth: 2, backgroundColor: "transparent", borderColor: "green", width: 10, height: 10, transform: [{ scale: bandth }] }}></Animated.View>
       {/* </View> */}
     </Animated.View>
   );
@@ -142,7 +208,7 @@ const styles = StyleSheet.create({
     textAlign: "center",
   },
   textFound: {
-    color:"white",
+    color: "white",
     textAlign: "center"
   }
 });
