0d052f5e30ca878e55045d71e49df63aeb550bbe
pedrotnascimento@gmail.com <pedrotnascimento@gmail.com>
2023-07-18 01:33:30 -0300

    dynamic background

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@
 
 import React, { useEffect, useState } from 'react';
 import {
+  Animated,
   StyleSheet,
   Text,
   TouchableOpacity,
@@ -15,7 +16,7 @@ import {
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 const catIcon = <MaterialCommunityIcons name={"cat"} size={128} />;
-const searchingIcon = <MaterialCommunityIcons name={"map-marker-question-outline"} size={128} />;
+const searchingIcon = <MaterialCommunityIcons name={"map-marker-question-outline"} size={128}  color={"green"}/>;
 
 var Sound = require('react-native-sound');
 
@@ -42,15 +43,17 @@ for (let i = 1; i <= TOTAL_CAT_SOUNDS; i++) {
   catSounds.push(cat);
 
 }
-const getRandomCatSoundInx = ()=>{
-  return Math.floor(Math.random()*TOTAL_CAT_SOUNDS);
-}
+const getRandomCatSoundInx = () => {
+  return Math.floor(Math.random() * TOTAL_CAT_SOUNDS);
+};
 const randomStartingInx = getRandomCatSoundInx();
 
 const App = () => {
   const [playing, setPlaying] = useState(false);
-  const [soundInx, setSoundInx] = useState(randomStartingInx);
- console.log(soundInx);
+  // const [soundInx, setSoundInx] = useState(randomStartingInx);
+  // console.log(soundInx);
+  const [dinamycBackgroundColor, setX] = useState(new Animated.Value(0));
+
   useEffect(() => {
     ding.setVolume(1);
     return () => {
@@ -60,15 +63,24 @@ const App = () => {
 
 
   const play = () => {
-    let currSound = catSounds[soundInx];
-    
+    let currSound = catSounds[randomStartingInx];
+
     if (!playing) {
       setPlaying(true);
+      Animated.spring(
+        dinamycBackgroundColor,
+        {
+
+          toValue: 1, useNativeDriver: true,
+          speed: 1,
+          bounciness: 100,
+        }
+      ).start();
       const cb = (success: any) => {
 
         if (success) {
-          const nextInx =getRandomCatSoundInx();
-          
+          const nextInx = getRandomCatSoundInx();
+
           const nextSound = catSounds[nextInx];
           currSound = nextSound;
           nextSound.play(cb);
@@ -80,24 +92,39 @@ const App = () => {
     }
     else {
       setPlaying(false);
+      Animated.spring(
+        dinamycBackgroundColor,
+        {
+
+          toValue: 0, useNativeDriver: true,
+          speed: 1,
+          bounciness: 100,
+        }
+      ).start();
       currSound.stop();
     }
   };
 
   const FoundedButton = <TouchableOpacity style={styles.playBtn} onPress={play}>
     {searchingIcon}
-    <Text style={styles.text}>Touch if found!</Text>
+    <Text style={styles.textFound}>Touch if found!</Text>
   </TouchableOpacity>;
 
   const FindButton = <TouchableOpacity style={styles.playBtn} onPress={play}>
     {catIcon}
     <Text style={styles.text}>Touch to find cat</Text>
   </TouchableOpacity>;
+  var color = dinamycBackgroundColor.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['rgba(255, 255, 255, 1)', 'rgba(0, 0, 0, 1)']
+  });
 
   return (
-    <View style={styles.container}>
+    // <View style={styles.container}>
+    <Animated.View style={{ ...styles.container, backgroundColor: color }}>
       {playing ? FoundedButton : FindButton}
-    </View>
+      {/* </View> */}
+    </Animated.View>
   );
 };
 
@@ -112,6 +139,10 @@ const styles = StyleSheet.create({
     // padding: 20,
   },
   text: {
+    textAlign: "center",
+  },
+  textFound: {
+    color:"white",
     textAlign: "center"
   }
 });
