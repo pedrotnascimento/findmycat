1d9c9032649dc477bef67acc367d81b491412e6d
pedrotnascimento@gmail.com <pedrotnascimento@gmail.com>
2023-09-12 23:38:44 -0300

    adding new sounds

--- MainFrame/soundLogic.ts
+++ MainFrame/soundLogic.ts
@@ -5,22 +5,28 @@ var Sound = require('react-native-sound');
 Sound.setCategory('Playback');
 
 export const catSounds: any[] = [];
-export const TOTAL_CAT_SOUNDS = 13;
+export const TOTAL_CAT_SOUNDS = 18;
 for (let i = 1; i <= TOTAL_CAT_SOUNDS; i++) {
-    const cat = new Sound(`cat${i}.mp3`, Sound.MAIN_BUNDLE, (error) => {
-        if (error) {
-            console.error('failed to load the sound', error);
-            return;
-        }
-    });
-    catSounds.push(cat);
+
+    try {
+        const cat = new Sound(`cat${i}.mp3`, Sound.MAIN_BUNDLE, (error) => {
+            if (error) {
+                console.error('failed to load the sound', i, error);
+                return;
+            }
+        });
+        catSounds.push(cat);
+    }
+    catch (e) {
+        console.error('Exception on loading cat sound', i);
+    }
 }
 
 
 export function getNextCatSound() {
     const nextInx = getRandomCatSoundInx(TOTAL_CAT_SOUNDS);
     const nextSound = catSounds[nextInx];
-    
-    
+
+
     return { nextInx, nextSound };
 }
